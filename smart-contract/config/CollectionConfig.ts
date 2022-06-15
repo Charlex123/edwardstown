e0461aced2d1766fbec89627d477e548fde202bc
2036380 <m.zandoamos@wlv.ac.uk>
2022-06-15 04:09:42 +0100

    initial commit

---
+++ smart-contract/config/CollectionConfig.ts
@@ -0,0 +1,34 @@
+import CollectionConfigInterface from '../lib/CollectionConfigInterface';
+import * as Networks from '../lib/Networks';
+import * as Marketplaces from '../lib/Marketplaces';
+import whitelistAddresses from './whitelist.json';
+
+const CollectionConfig: CollectionConfigInterface = {
+  testnet: Networks.ethereumTestnet,
+  mainnet: Networks.ethereumMainnet,
+  // The contract name can be updated using the following command:
+  // yarn rename-contract NEW_CONTRACT_NAME
+  // Please DO NOT change it manually!
+  contractName: 'EdwardsTown',
+  tokenName: 'EdwardsTown',
+  tokenSymbol: 'EDWT',
+  maxSupply: 10000,
+  whitelistSale: {
+    price: 0.00,
+    maxMintAmountPerTx: 1,
+  },
+  preSale: {
+    price: 0.00,
+    maxMintAmountPerTx: 1,
+  },
+  publicSale: {
+    price: 0.0055,
+    maxMintAmountPerTx: 5,
+  },
+  contractAddress: "0x460d68754039480e4Ab0b7414659C95Aa446bAEF",
+  marketplaceIdentifier: 'my-nft-token',
+  marketplaceConfig: Marketplaces.openSea,
+  whitelistAddresses,
+};
+
+export default CollectionConfig;
